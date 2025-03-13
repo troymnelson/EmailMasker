8ff7f28016b3b2cb1c94d594b80a0d7b167883a2
Troy Nelson <troymn625@gmail.com>
2025-03-12 20:20:51 -0500

    3/12/25 eod

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
 const inputEl = document.getElementById('email');
-
+const submitBtn = document.getElementById('submitBtn');
+const formEl = document.getElementById('mask-this');
 
 const maskEmail = (email) => {
   let emailUsername = email.slice(0, email.indexOf("@"));
-  let partOfEmailToMask = email.slice(1, -1);
+  let partOfEmailToMask = emailUsername.slice(1, -1);
   let maskedEmail = emailUsername.replace(partOfEmailToMask, "*".repeat(partOfEmailToMask.length));
-  console.log(maskedEmail);
+  let newEmail = maskedEmail + email.slice(email.indexOf('@'));
+  return newEmail;
+
 }
 
-maskEmail("example@test.com");
+submitBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  let result = maskEmail(inputEl.value);
+  const newEl = document.createElement('p');
+  newEl.textContent = `Masked Email: ${result}`
+  formEl.appendChild(newEl);
+});
+
